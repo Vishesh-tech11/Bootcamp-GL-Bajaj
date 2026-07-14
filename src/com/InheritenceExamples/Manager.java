12c43e18179907128ae39968de7fcd8fa73bbfa9
Vishesh singh Bhadauria <visheshsinghbhadauria@Visheshs-MacBook-Air.local>
2026-07-14 10:05:47 +0530

    OOPs Inheritance Assignment

---
+++ src/com/InheritenceExamples/Manager.java
@@ -0,0 +1,13 @@
+package com.InheritenceExamples;
+
+public class Manager extends Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Manager obj = new Manager();
+		System.out.println("Name:" + obj.name);
+		System.out.println("EmployeeId:" + obj.employeeID);
+		System.out.println("Salary:" + obj.salary);
+	}
+
+}
---
+++ src/com/InheritenceExamples/Product.java
@@ -0,0 +1,10 @@
+package com.InheritenceExamples;
+
+public class Product {
+	protected int productId = 101;
+	protected String name = "Laptop";
+	protected double price = 65000;
+	protected int stockQuantity = 15;
+	protected String category = "Electronics";
+
+}
