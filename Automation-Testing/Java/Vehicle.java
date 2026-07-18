28bfc3002db59c8d6677ac8efb1c0f4b00bf35cf
Vishesh <visheshsinghbhadauriya547@gmail.com>
2026-07-18 05:42:08 +0530

    Delete Automation-Testing/Java directory

--- Automation-Testing/Java/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package com.InheritenceExamples;
-
-public class Vehicle {
-	protected String vehicleNumber = "UP32AB1234";
-	protected String model = "Swift";
-	protected double rentalRatePerDay = 1500;
-	protected boolean isAvailable = true;
-
-}
